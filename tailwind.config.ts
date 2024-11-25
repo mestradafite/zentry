bf8a0ac767e831dbf57f44fa5eefc1dd01bb3258
mestrada <mestrada@gmail.com>
2024-11-25 21:50:01 +0100

    Hero section & uploaded videos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        zentry: ['zentry', 'sans-serif'],
+        general: ['general', 'sans-serif'],
+        'circular-web': ['circular-web', 'sans-serif'],
+        'robert-medium': ['robert-medium', 'sans-serif'],
+        'robert-regular': ['robert-regular', 'sans-serif'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          50: '#DFDFF0',
+          75: '#dfdff2',
+          100: '#F0F2FA',
+          200: '#010101',
+          300: '#4FB7DD',
+        },
+        violet: {
+          300: '#5724ff',
+        },
+        yellow: {
+          100: '#8e983f',
+          300: '#edff66',
+        },
       },
     },
   },
